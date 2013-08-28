750da39d99ce0898d3dcd9cde5efd041abfaa7cf
James Glover <jg16@sanger.ac.uk>
2013-08-28 11:56:52 +0100

    Add default printers and label counts

--- app/models/presenters/final_pooled_presenter.rb
+++ app/models/presenters/final_pooled_presenter.rb
@@ -69,4 +69,8 @@ class Presenters::FinalPooledPresenter < Presenters::PooledPresenter
   def tube_state=(state)
     # Ignore this
   end
+
+  def default_tube_printer_uuid
+    Settings.printers[location][Settings.purposes[default_child_purpose.uuid].default_printer_type]
+  end
 end
